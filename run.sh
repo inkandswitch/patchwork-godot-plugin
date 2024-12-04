14009ec02eff10c1012ff77b6e6171063c4a3e04
Paul Sonnentag <paul.sonnentag@googlemail.com>
2024-12-04 10:43:22 +0100

    move games out of repo

--- run.sh
+++
@@ -1,7 +0,0 @@
-# Build the Rust plugin
-cd rust/plugin
-cargo build || exit 1
-
-# Change back to root directory and open Godot project
-cd ../..
-/Applications/Godot.app/Contents/MacOS/Godot moddable-pong/project.godot
