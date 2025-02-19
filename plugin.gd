5158303860a12ca5ac91e2585c625c900b253f07
nikitalita <69168929+nikitalita@users.noreply.github.com>
2025-02-18 22:43:54 -0800

    fix connection

--- plugin.gd
+++ plugin.gd
@@ -68,7 +68,7 @@ func _process(_delta: float) -> void:
 				# reload scene files to update references
 				get_editor_interface().reload_scene_from_path(path)
 		if files_to_reimport.size() > 0:
-			EditorInterface.get_resource_filesystem().reimport_files(files_to_reimport)
+			EditorInterface.get_resource_filesystem().reimport_files(files_to_reimport.keys())
 		if should_rerun:
 			timer = get_tree().create_timer(5, true)
 			timer.timeout.connect(self.sync_patchwork_to_godot)
