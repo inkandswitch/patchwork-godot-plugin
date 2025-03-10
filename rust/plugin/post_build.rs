9e58686926795e9778ac6007b4d1ce2741a45d96
nikitalita <69168929+nikitalita@users.noreply.github.com>
2025-03-10 16:00:23 -0700

    Add rust plugin CI

--- rust/plugin/post_build.rs
+++ rust/plugin/post_build.rs
@@ -13,6 +13,10 @@ fn after_build(){
     let cwd = env::var("CRATE_MANIFEST_DIR").unwrap();
     let cwd_path = Path::new(&cwd);
     let lastbuild = fs::read_to_string(cwd_path.join("target/.lastbuild")).unwrap();
+
+    // is_ok and is 1
+    let isCI = env::var("CI").is_ok() && env::var("CI").unwrap() == "1";
+    println!("cargo:warning=CI={}", isCI);
     
     // Parse the file contents
     let out_dir = lastbuild.split("OUT_DIR=").nth(1).unwrap().split("\n").next().unwrap();
@@ -23,6 +27,15 @@ fn after_build(){
     println!("cargo:warning=PROFILE={}", profile);
     println!("cargo:warning=TARGET={}", target);
     println!("cargo:warning=CWD={}", cwd);
+
+    // arch is x86_64 or arm64 depending on the target
+    let arch_macos = if target.contains("x86_64") {
+        "x86_64"
+    } else if target.contains("arm64") || target.contains("aarch64") {
+        "arm64"
+    } else {
+        panic!("Unsupported target architecture: {}", target);
+    };
 
     // Determine the platform name from the target triple
     let platform_name = if target.contains("darwin") {
@@ -38,12 +51,12 @@ fn after_build(){
     let mut target_dirs = vec![target_dir.to_path_buf()];
     let mut targets = vec![target];
     // Get the library name and extension based on platform
-    let (lib_name, lib_ext) = if target.contains("windows") {
-        ("patchwork_rust_core", "dll")
+    let (lib_name, lib_dll_ext, lib_a_ext) = if target.contains("windows") {
+        ("patchwork_rust_core", "dll", "lib")
     } else if target.contains("darwin") {
-        ("libpatchwork_rust_core", "dylib")
+        ("libpatchwork_rust_core", "dylib", "a")
     } else {
-        ("libpatchwork_rust_core", "so")
+        ("libpatchwork_rust_core", "so", "a")
     };
     // if this platform_name is macos, and the target is aarch64-apple-darwin, write a file to the target directory called ".nextbuild"
     // check if this exists first
@@ -99,7 +112,8 @@ fn after_build(){
     let size = target_dirs.len();
     for (i, target_dir) in target_dirs.iter().enumerate() {
         // Construct paths
-        let lib_path = target_dir.join(format!("{}.{}", lib_name, lib_ext));
+        let dll_lib_path = target_dir.join(format!("{}.{}", lib_name, lib_dll_ext));
+        let a_lib_path = target_dir.join(format!("{}.{}", lib_name, lib_a_ext));
 
         let platform_dir = Path::new(&cwd_path).join(platform_name);
 
@@ -107,25 +121,39 @@ fn after_build(){
         fs::create_dir_all(&platform_dir).unwrap();
         println!("cargo:warning=target_dir directory {:?}", target_dir);
         // Copy the library to the platform-specific directory
-        println!("cargo:warning=Copying library from {:?} to {:?}", lib_path, platform_dir);
-
+        println!("cargo:warning=Copying library from {:?} to {:?}", dll_lib_path, platform_dir);
+        println!("cargo:warning=Copying library from {:?} to {:?}", a_lib_path, platform_dir);
         
 
-        let dest_path = if platform_name == "macos" {
+        let dll_dest_path = if platform_name == "macos" {
             // it goes in the "macos/libpatchwork_rust_core.macos.framework" directory
-            // platform_dir.join(format!("{}.{}.{}", lib_name, targets[i], lib_ext))
-            platform_dir.join(format!("{}.macos.framework", &lib_name)).join(format!("{}.{}", lib_name, lib_ext))
+            // platform_dir.join(format!("{}.{}.{}", lib_name, targets[i], lib_dll_ext))
+            // if isCI {
+            //     platform_dir.join(format!("{}.macos.framework", &lib_name)).join(format!("{}.{}", lib_name, lib_dll_ext))
+            // } else {
+                platform_dir.join(format!("{}.macos.framework", &lib_name)).join(format!("{}.{}", lib_name, lib_dll_ext))
+            // }
         } else {
-            platform_dir.join(format!("{}.{}.{}", lib_name, targets[i], lib_ext))
+            platform_dir.join(format!("{}.{}.{}", lib_name, targets[i], lib_dll_ext))
+        };
+        let a_dest_path = if platform_name == "macos" {
+            platform_dir.join(format!("{}.macos.framework", &lib_name)).join(format!("{}.{}.{}", lib_name, arch_macos, lib_a_ext))
+        } else {
+            platform_dir.join(format!("{}.{}.{}", lib_name, targets[i], lib_a_ext))
         };
         // check that libpath exists
-        if !lib_path.exists() {
-            panic!("Library file does not exist: {:?}", lib_path);
+        if !dll_lib_path.exists() {
+            panic!("Library file does not exist: {:?}", dll_lib_path);
         }
-        fs::copy(&lib_path, &dest_path).unwrap();
+        if !a_lib_path.exists() {
+            panic!("Library file does not exist: {:?}", a_lib_path);
+        }
+        fs::copy(&dll_lib_path, &dll_dest_path).unwrap();
+        fs::copy(&a_lib_path, &a_dest_path).unwrap();
 
         println!("cargo:rerun-if-changed=build.rs");
-        println!("cargo:warning=Copied library to {:?}", dest_path);
+        println!("cargo:warning=Copied library to {:?}", dll_dest_path);
+        println!("cargo:warning=Copied library to {:?}", a_dest_path);
     }
 }
 
