2ce5908857001f35f5fa56156b7ddbd18b3e657a
nikitalita <69168929+nikitalita@users.noreply.github.com>
2025-03-12 23:14:00 -0700

    expose DiffInspector to gdscript

--- register_types.cpp
+++ register_types.cpp
@@ -4,9 +4,11 @@
 
 #include "register_types.h"
 #include "editor/PEEditorInspector.h"
+#include "editor/diff_inspector.h"
 #include "editor/editor_node.h"
 #include "editor/missing_resource_container.h"
 #include "editor/patchwork_editor.h"
+
 void patchwork_editor_init_callback() {
 	EditorNode *editor = EditorNode::get_singleton();
 	editor->add_child(memnew(PatchworkEditor(editor)));
@@ -17,6 +19,7 @@ void initialize_patchwork_editor_module(ModuleInitializationLevel p_level) {
 		EditorNode::add_init_callback(&patchwork_editor_init_callback);
 	}
 	if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE) {
+		ClassDB::register_class<DiffInspector>();
 		ClassDB::register_class<PatchworkEditor>();
 		ClassDB::register_class<FakeInspectorResource>();
 		ClassDB::register_class<PEEditorInspector>();
