2b6192e9c3dd74f26cb7054c5741f95ed3c81279
Paul Sonnentag <paul.sonnentag@googlemail.com>
2025-03-19 00:37:06 -0700

    order properties

--- rust/plugin/src/test.rs
+++ rust/plugin/src/test.rs
@@ -86,27 +86,27 @@ position = Vector2(1920, 576)
 
     let reserialized = r#"[gd_scene load_steps=14 format=4 uid="uid://dhcpt1kt8cs0g"]
 
-[ext_resource uid="uid://jnrusvm3gric" type="PackedScene" path="res://spaces/background.tscn" id="2_tb5a2"]
-[ext_resource uid="uid://dthyncc3spfks" type="PackedScene" path="res://spaces/dangerzone.tscn" id="4_2mg6t"]
+[ext_resource type="PackedScene" path="res://spaces/tilemap.tscn" uid="uid://danlrmsfmsros" id="3_yfnmv"]
+[ext_resource path="res://components/player/player.tscn" type="PackedScene" uid="uid://8st4scqt06l8" id="2_7yl00"]
+[ext_resource path="res://spaces/background.tscn" type="PackedScene" uid="uid://jnrusvm3gric" id="2_tb5a2"]
+[ext_resource uid="uid://bjqboxowe07lw" path="res://assets/items/crystal.png" type="Texture2D" id="6_kpi5m"]
+[ext_resource path="res://components/platform/platform.tscn" uid="uid://coq6d3u6wnvs2" type="PackedScene" id="4_gd51l"]
+[ext_resource uid="uid://beuisy5yrw0bq" type="PackedScene" path="res://components/flag/flag.tscn" id="12_dkbog"]
+[ext_resource path="res://scripts/multiplayer_camera.gd" type="Script" id="13_0d2mj"]
 [ext_resource path="res://components/player/spriteframes-red.tres" uid="uid://bo581k1esb50n" type="SpriteFrames" id="9_qmofe"]
-[ext_resource type="PackedScene" uid="uid://8st4scqt06l8" path="res://components/player/player.tscn" id="2_7yl00"]
-[ext_resource uid="uid://jt80gv02u4f2" type="PackedScene" path="res://hud.tscn" id="6_mp7wy"]
-[ext_resource type="Script" path="res://scripts/multiplayer_camera.gd" id="13_0d2mj"]
-[ext_resource type="PackedScene" uid="uid://cswheshouik14" path="res://rules_goals/game_logic.tscn" id="3_xbkvd"]
-[ext_resource type="Texture2D" path="res://assets/items/crystal.png" uid="uid://bjqboxowe07lw" id="6_kpi5m"]
-[ext_resource type="PackedScene" uid="uid://beuisy5yrw0bq" path="res://components/flag/flag.tscn" id="12_dkbog"]
-[ext_resource path="res://spaces/tilemap.tscn" type="PackedScene" uid="uid://danlrmsfmsros" id="3_yfnmv"]
-[ext_resource type="PackedScene" uid="uid://daf24t18h3n5e" path="res://components/coin/coin.tscn" id="5_u7hr5"]
-[ext_resource uid="uid://coq6d3u6wnvs2" type="PackedScene" path="res://components/platform/platform.tscn" id="4_gd51l"]
+[ext_resource path="res://hud.tscn" uid="uid://jt80gv02u4f2" type="PackedScene" id="6_mp7wy"]
+[ext_resource path="res://rules_goals/game_logic.tscn" type="PackedScene" uid="uid://cswheshouik14" id="3_xbkvd"]
 [ext_resource type="PackedScene" uid="uid://dk0xon0k7ga23" path="res://components/enemy/enemy.tscn" id="9_l6smt"]
+[ext_resource uid="uid://dthyncc3spfks" type="PackedScene" path="res://spaces/dangerzone.tscn" id="4_2mg6t"]
+[ext_resource uid="uid://daf24t18h3n5e" path="res://components/coin/coin.tscn" type="PackedScene" id="5_u7hr5"]
 
 [node name="Main" type="Node2D"]
 metadata/patchwork_id="cb69ec7e34864e5ca2bd4d0ca7105834"
 
 [node name="GameLogic" parent="." instance=ExtResource("3_xbkvd")]
-win_by_reaching_flag=true
 metadata/patchwork_id="d371a8c3887747538234d2300e26fc1f"
 win_by_collecting_coins=true
+win_by_reaching_flag=true
 
 [node name="Background" parent="." instance=ExtResource("2_tb5a2")]
 metadata/patchwork_id="14d0420c1c0844b582d2e6fac7569249"
@@ -120,24 +120,24 @@ position=Vector2(3072, 1216)
 metadata/patchwork_id="6b9b86bf04b64b958a043bc3444f2d88"
 
 [node name="Coin" parent="Coins" instance=ExtResource("5_u7hr5")]
-texture=ExtResource("6_kpi5m")
 metadata/patchwork_id="486819e024244c9880bee32c0a846a08"
-position=Vector2(1472, 320)
-tint=Color(1, 1, 0, 1)
 modulate=Color(1, 1, 0, 1)
+position=Vector2(1472, 320)
+texture=ExtResource("6_kpi5m")
+tint=Color(1, 1, 0, 1)
 
 [node name="Coin3" parent="Coins" instance=ExtResource("5_u7hr5")]
-modulate=Color(1, 1, 0, 1)
-tint=Color(1, 1, 0, 1)
-position=Vector2(1600, 256)
 metadata/patchwork_id="ff095b825e7642739a37e2329f1e30fb"
+modulate=Color(1, 1, 0, 1)
+position=Vector2(1600, 256)
 texture=ExtResource("6_kpi5m")
+tint=Color(1, 1, 0, 1)
 
 [node name="Coin4" parent="Coins" instance=ExtResource("5_u7hr5")]
 metadata/patchwork_id="48e4d518129f4cdaad74075a7ae458f1"
-texture=ExtResource("6_kpi5m")
-position=Vector2(1728, 320)
 modulate=Color(1, 1, 0, 1)
+position=Vector2(1728, 320)
+texture=ExtResource("6_kpi5m")
 tint=Color(1, 1, 0, 1)
 
 [node name="Platforms" type="Node2D" parent="."]
@@ -145,30 +145,30 @@ metadata/patchwork_id="a780e1716d0641e5a6577a23f3d75932"
 position=Vector2(1920, -64)
 
 [node name="Platform" parent="Platforms" instance=ExtResource("4_gd51l")]
-width=2
 metadata/patchwork_id="a9c3a5a969054291a512136c4baee35a"
 position=Vector2(960, 320)
+width=2
 
 [node name="Platform4" parent="Platforms" instance=ExtResource("4_gd51l")]
-width=2
 fall_time=2.0
-one_way=true
 metadata/patchwork_id="ee10dc07f8fa42b29aa5b3249b6758d8"
+one_way=true
 position=Vector2(-1472, 0)
+width=2
 
 [node name="Player" parent="." instance=ExtResource("2_7yl00")]
-sprite_frames=ExtResource("9_qmofe")
-position=Vector2(512, 576)
 collision_layer=1
-metadata/patchwork_id="6c9f6bc542514ca18fc1a24c0a734a19"
 collision_mask=7
+metadata/patchwork_id="6c9f6bc542514ca18fc1a24c0a734a19"
+position=Vector2(512, 576)
+sprite_frames=ExtResource("9_qmofe")
 
 [node name="Camera2D" type="Camera2D" parent="Player"]
-metadata/patchwork_id="31ae337cfea242bfada4d2a1786b32f1"
-position_smoothing_enabled=true
 limit_bottom=1080
 limit_left=0
+metadata/patchwork_id="31ae337cfea242bfada4d2a1786b32f1"
 position=Vector2(0, 15)
+position_smoothing_enabled=true
 
 [node name="HUD" parent="." instance=ExtResource("6_mp7wy")]
 metadata/patchwork_id="a7f5c2a130bf4602b589000a672769c7"
@@ -178,9 +178,8 @@ metadata/patchwork_id="b0927783ef0a4241b1ffa30f86bf63fa"
 position=Vector2(1600, 704)
 
 [node name="Enemy2" parent="." instance=ExtResource("9_l6smt")]
-position=Vector2(1920, 576)
 metadata/patchwork_id="431d07788c4242d6a54b7bea25dd2357"
-"#;
+position=Vector2(1920, 576)"#;
 
     let scene = godot_parser::parse_scene(&reserialized.to_string()).unwrap();
 
