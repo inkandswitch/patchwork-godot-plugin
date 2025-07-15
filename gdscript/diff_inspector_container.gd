936422e40ccb73deb40aebddf2178115487109a7
nikitalita <69168929+nikitalita@users.noreply.github.com>
2025-07-14 23:13:30 -0700

    fix selecting resource and scene when clicking

--- gdscript/diff_inspector_container.gd
+++ gdscript/diff_inspector_container.gd
@@ -280,28 +280,25 @@ func _on_resource_box_clicked(section: String) -> void:
 	EditorInterface.get_file_system_dock().navigate_to_path(file_path)
 	if extension == "tscn" or extension == "scn":
 		var curr_root = EditorInterface.get_edited_scene_root()
-		if curr_root.scene_file_path == file_path:
+		if is_instance_valid(curr_root) && curr_root.scene_file_path == file_path:
 			pass
-			# var curr_obj = EditorInterface.get_inspector().get_edited_object()
-			# var reedit = false
-			# if curr_obj != curr_root:
-			# 	# print("NOT THE SAME OBJECT")
-			# 	reedit = true
-			# 	if curr_obj is Node && curr_root.is_ancestor_of(curr_obj):
-			# 		reedit = false
-			# if reedit:
-			# 	EditorInterface.inspect_object(curr_root, "", false)
+			var curr_obj = EditorInterface.get_inspector().get_edited_object()
+			var reedit = false
+			if curr_obj != curr_root:
+				# print("NOT THE SAME OBJECT")
+				reedit = true
+				if curr_obj is Node && curr_root.is_ancestor_of(curr_obj):
+					reedit = false
+			if reedit:
+				EditorInterface.inspect_object(curr_root, "", false)
 		else:
 			EditorInterface.open_scene_from_path(file_path)
 		# last_inspected_resource = null
 		EditorInterface.set_main_screen_editor("2D")
 	else:
-		pass
-		# TODO: re-enable this, it's causing a crash when switching branches
-		# var ff = ResourceLoader.load(file_path)
-		# if is_instance_valid(ff):
-		# 	EditorInterface.inspect_object(ff, "", true)
-			# last_inspected_resource = ff
+		var ff = ResourceLoader.load(file_path)
+		if is_instance_valid(ff):
+			EditorInterface.edit_resource(ff)
 
 func _on_text_box_clicked(section: String) -> void:
 	var file_path = section
