2b877e8b40a633993df64964e1839e2a39fe3c4c
nikitalita <69168929+nikitalita@users.noreply.github.com>
2026-01-30 19:06:10 -0800

    remove unused stuff in PatchworkEditor

--- editor/register_types.cpp
+++ editor/register_types.cpp
@@ -3,25 +3,13 @@
 /*************************************************************************/
 
 #include "register_types.h"
-#include "editor/editor_node.h"
 #include "patchwork_editor.h"
 
-void patchwork_editor_init_callback() {
-	EditorNode *editor = EditorNode::get_singleton();
-	editor->add_child(memnew(PatchworkEditor(editor)));
-}
 
 void initialize_patchwork_editor_module(ModuleInitializationLevel p_level) {
-	if (p_level == MODULE_INITIALIZATION_LEVEL_EDITOR) {
-		EditorNode::add_init_callback(&patchwork_editor_init_callback);
-	}
 	if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE) {
 		ClassDB::register_class<PatchworkEditor>();
-		// register the editor inspector section
 	}
-
-
-
 }
 
 void uninitialize_patchwork_editor_module(ModuleInitializationLevel p_level) {
