59af646ef68fd64e968ef29874b351f2820794a9
nikitalita <69168929+nikitalita@users.noreply.github.com>
2026-02-12 22:02:12 -0800

    push test to ensure that windows-style paths get ignored properly

--- rust/src/project/driver.rs
+++ rust/src/project/driver.rs
@@ -544,3 +544,17 @@ impl DriverInner {
         }
     }
 }
+
+// tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_should_ignore_windows_style_paths() {
+        let dir = PathBuf::from("C:\\foo\\bar\\");
+        let gitignore = Driver::build_gitignore(&dir);
+        // ignores windows style paths
+        assert!(gitignore.matched_path_or_any_parents(dir.join(".patchwork\\thingy.txt").as_path(), false).is_ignore());
+        assert!(!gitignore.matched_path_or_any_parents(dir.join("blargh\\baz.txt").as_path(), false).is_ignore());
+    }
+}
