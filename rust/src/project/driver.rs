cfdcab1dc5f16a44a95be7f0ae693d70b2e46bd0
Lilith Silver <84940819+LilithSilver@users.noreply.github.com>
2026-02-19 23:01:29 -0800

    move tests to external files

--- rust/src/project/driver.rs
+++ rust/src/project/driver.rs
@@ -24,6 +24,9 @@ use tokio::sync::{Mutex, mpsc, watch};
 use tokio_util::sync::CancellationToken;
 use tracing::instrument;
 
+#[cfg(test)]
+mod tests;
+
 /// The main driver for the project.
 /// Hooks together all the various controllers.
 /// When this object is constructed, it is started. When the handle is dropped, it shuts down.
@@ -537,27 +540,5 @@ impl DriverInner {
         } else {
             Vec::new()
         }
-    }
-}
-
-// tests
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn test_should_ignore_windows_style_paths() {
-        let dir = PathBuf::from("C:\\foo\\bar\\");
-        let gitignore = Driver::build_gitignore(&dir);
-        // ignores windows style paths
-        assert!(
-            gitignore
-                .matched_path_or_any_parents(dir.join(".patchwork\\thingy.txt").as_path(), false)
-                .is_ignore()
-        );
-        assert!(
-            !gitignore
-                .matched_path_or_any_parents(dir.join("blargh\\baz.txt").as_path(), false)
-                .is_ignore()
-        );
     }
 }
